f489930936f5eed497ce94dcd062f3973adf693a
chin8628 <boon.8628@hotmail.com>
2015-05-26 23:25:30 +0700

    Add Waypoint's script

---
+++ asset/js/script.js
@@ -0,0 +1,18 @@
+$(window).load(function(){
+   $('#loading-page').fadeOut(1000);
+});
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+    $('#btn-top').hide();
+
+    var inview = new Waypoint.Inview({
+	  	element: $('.footer')[0],
+	  	entered: function() {
+	    	$('#btn-top').fadeIn(200);
+	  	},
+	  	exited: function() {
+	    	$('#btn-top').fadeOut(200);
+		}
+	})
+});
