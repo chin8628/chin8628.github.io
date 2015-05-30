d28f7d332d7b249db458464897edc993e4483c41
chin8628 <boon.8628@hotmail.com>
2015-05-30 22:09:57 +0700

    Add fade-in's animate in header

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,10 @@
 $(window).load(function(){
-   $('#loading-page').fadeOut(1000);
+   	$('#loading-page').fadeOut(1000 ,function(){
+		$('#profile').fadeIn(3000);
+		$('.name-website').fadeIn(3000);
+		$('.describe').fadeIn(3000);
+		$('.hashtag').fadeIn(3000);
+   	});
 });
 
 $(document).ready(function(){
@@ -7,6 +12,8 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     $('#btn-top').hide();
     $('.modal-trigger').leanModal();
+
+
 
     var inview = new Waypoint.Inview({
 	  	element: $('.footer')[0],
