f78053c147f4578403c5e79450097180d8bf4d86
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2023-09-04 23:51:59 +0700

    Refactor a gallery component

---
+++ components/dialog/dialog.js
@@ -0,0 +1,22 @@
+customElements.define(
+    "gallery-thumbnail",
+    class extends HTMLElement {
+        async connectedCallback() {
+            const res = await fetch("components/dialog/index.html")
+            const shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.innerHTML = await res.text();
+
+            const src = this.getAttribute("src")
+            const img = shadowRoot.getElementById("thumbnail")
+            const dialog = shadowRoot.getElementById("full-photo-dialog")
+            img.setAttribute("src", src)
+            img.addEventListener('click', (event) => {
+                dialog.showModal()
+            })
+
+            const fullImgSrc = this.getAttribute("full-src")
+            const dialogFullPhoto = shadowRoot.getElementById("full-photo")
+            dialogFullPhoto.setAttribute("src", fullImgSrc)
+        }
+    },
+);
